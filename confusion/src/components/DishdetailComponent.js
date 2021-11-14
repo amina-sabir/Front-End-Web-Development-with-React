7935df0b4e3056faeb7c724abc65361a625842c0
amina-sabir <aminasabir8324@gmail.com>
2021-11-14 19:55:30 +0500

    Assignment No. 1

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardTitle, CardBody} from 'reactstrap';
+
+class DishDetail extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    renderDish(dish){
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+    }
+
+    renderComments(cmt){
+
+        const commentBook = cmt.map((comment) => {
+            return(
+            <li key={comment.id}>
+            <p>{comment.comment}</p>
+            <p>--- {comment.author},
+            {comment.date}
+            </p>
+            </li>
+            )
+        });
+
+        if(cmt != null){
+            return(
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {commentBook}
+                </ul>
+            </div>
+            );
+        }
+        else{
+            return <div></div>;
+        }
+    }
+
+    render(){
+    const dish = this.props.dish;
+
+    if( dish != null){
+    return (
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                {this.renderDish(dish)}
+            </div>
+            {this.renderComments(dish.comments)}
+        </div>
+    );
+    }
+    else{ 
+        return <div></div> 
+    }
+    }  
+}
+
+export default DishDetail;
