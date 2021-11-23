0e37521ff273d0d0ebec85903a8865ba1830711b
amina-sabir <aminasabir8324@gmail.com>
2021-11-23 21:35:04 +0500

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React , { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from "./FooterComponent";
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { Route , Switch, Redirect} from 'react-router-dom';
 
 
 class Main extends Component{
@@ -12,21 +14,31 @@ class Main extends Component{
 
         this.state = {
             dishes: DISHES,
-            selectedDish:null
+            // selectedDish:null
         };
     }
     
-    onDishSelect(dishId){
-        this.setState({
-            selectedDish: dishId });
-    }
+    // onDishSelect(dishId){
+    //     this.setState({
+    //         selectedDish: dishId });
+    // }
 
     render(){
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
         return (
             <div>
               <Header />
-              <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+              {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+              <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+                <Redirect to="/home"/>
+              </Switch>
               <Footer />
             </div>
   );
